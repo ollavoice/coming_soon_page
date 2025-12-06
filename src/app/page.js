ab0a2fbb2354e60b0e26d2859c82f92c0e59ba05
baramsivaramireddy <baramsivarami@gmail.com>
2025-12-07 00:35:27 +0530

    feat: basic

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <main>
+
+      {/*  */}
+
+      <div>
+        <p>Coming soon</p>
+      </div>
+    </main>
+    </>
+  );
+}
